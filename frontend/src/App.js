b814bc04b6d6701aa8df4527d8c9fd3c3828009a
Ha Cao Thu <ha.caothu.03@gmail.com>
2024-06-09 16:34:26 +0700

    shop frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/animals' element={<ShopCategory category="animals"/>}/>
+        <Route path='/dolls' element={<ShopCategory category="dolls"/>}/>
+        <Route path='/tools' element={<ShopCategory category="tools"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productID' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Assets/all_products.js
@@ -0,0 +1,566 @@
+
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.png";
+import p19_img from "./product_19.png";
+import p20_img from "./product_20.png";
+import p21_img from "./product_21.png";
+import p22_img from "./product_22.png";
+import p23_img from "./product_23.png";
+import p24_img from "./product_24.png";
+import p25_img from "./product_25.png";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p28_img from "./product_28.png";
+import p29_img from "./product_29.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
+import p32_img from "./product_32.png";
+import p33_img from "./product_33.png";
+import p34_img from "./product_34.png";
+import p35_img from "./product_35.png";
+import p36_img from "./product_36.png";
+import p37_img from "./product_37.png";
+import p38_img from "./product_38.png";
+import p39_img from "./product_39.png";
+import p40_img from "./product_40.png";
+import p41_img from "./product_41.png";
+import p42_img from "./product_42.png";
+import p43_img from "./product_43.png";
+import p44_img from "./product_44.png";
+import p45_img from "./product_45.png";
+import p46_img from "./product_46.png";
+import p47_img from "./product_47.png";
+import p48_img from "./product_48.png";
+import p49_img from "./product_49.png";
+import p50_img from "./product_50.png";
+import p51_img from "./product_51.png";
+import p52_img from "./product_52.png";
+import p53_img from "./product_53.png";
+import p54_img from "./product_54.png";
+import p55_img from "./product_55.png";
+import p56_img from "./product_56.png";
+import p57_img from "./product_57.png";
+import p58_img from "./product_58.png";
+import p59_img from "./product_59.png";
+import p60_img from "./product_60.png";
+import p61_img from "./product_61.png";
+
+let all_product = [
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p3_img,
+        new_price: 60.0,
+        old_price: 100.5,
+      },
+      {
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p4_img,
+        new_price: 100.0,
+        old_price: 150.0,
+      },
+      {
+        id: 5,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p5_img,
+        new_price: 80.0,
+        old_price: 120.0,
+      },
+      {
+        id: 6,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p6_img,
+        new_price: 75.0,
+        old_price: 130.0,
+      },
+      {
+        id: 7,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p7_img,
+        new_price: 95.0,
+        old_price: 145.0,
+      },
+      {
+        id: 8,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p8_img,
+        new_price: 70.0,
+        old_price: 110.0,
+      },
+      {
+        id: 9,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p9_img,
+        new_price: 85.0,
+        old_price: 125.0,
+      },
+      {
+        id: 10,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p10_img,
+        new_price: 90.0,
+        old_price: 140.0,
+      },
+      {
+        id: 11,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p11_img,
+        new_price: 65.0,
+        old_price: 105.0,
+      },
+      {
+        id: 12,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p12_img,
+        new_price: 78.0,
+        old_price: 118.0,
+      },
+      {
+        id: 13,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p13_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 14,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p14_img,
+        new_price: 88.0,
+        old_price: 128.0,
+      },
+      {
+        id: 15,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p15_img,
+        new_price: 77.0,
+        old_price: 117.0,
+      },
+      {
+        id: 16,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p16_img,
+        new_price: 82.0,
+        old_price: 122.0,
+      },
+      {
+        id: 17,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "animals",
+        image: p17_img,
+        new_price: 94.0,
+        old_price: 134.0,
+      },
+      
+      {
+        id: 18,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p18_img,
+        new_price: 90.0,
+        old_price: 130.0,
+      },
+      {
+        id: 19,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p19_img,
+        new_price: 95.0,
+        old_price: 135.0,
+      },
+      {
+        id: 20,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p20_img,
+        new_price: 85.0,
+        old_price: 125.0,
+      },
+      {
+        id: 21,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p21_img,
+        new_price: 88.0,
+        old_price: 128.0,
+      },
+      {
+        id: 22,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p22_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 23,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p23_img,
+        new_price: 87.0,
+        old_price: 127.0,
+      },
+      {
+        id: 24,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p24_img,
+        new_price: 89.0,
+        old_price: 129.0,
+      },
+      {
+        id: 25,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p25_img,
+        new_price: 91.0,
+        old_price: 131.0,
+      },
+      {
+        id: 26,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p26_img,
+        new_price: 86.0,
+        old_price: 126.0,
+      },
+      {
+        id: 27,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p27_img,
+        new_price: 93.0,
+        old_price: 133.0,
+      },
+      {
+        id: 28,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p28_img,
+        new_price: 84.0,
+        old_price: 124.0,
+      },
+      {
+        id: 29,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p29_img,
+        new_price: 96.0,
+        old_price: 136.0,
+      },
+      {
+        id: 30,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p30_img,
+        new_price: 89.0,
+        old_price: 129.0,
+      },
+      {
+        id: 31,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p31_img,
+        new_price: 94.0,
+        old_price: 134.0,
+      },
+      {
+        id: 32,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p32_img,
+        new_price: 90.0,
+        old_price: 130.0,
+      },
+      {
+        id: 33,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p33_img,
+        new_price: 85.0,
+        old_price: 125.0,
+      },
+      {
+        id: 34,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p34_img,
+        new_price: 91.0,
+        old_price: 131.0,
+      },
+      {
+        id: 35,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p35_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 36,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p36_img,
+        new_price: 88.0,
+        old_price: 128.0,
+      },
+      {
+        id: 37,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p37_img,
+        new_price: 94.0,
+        old_price: 134.0,
+      },
+      {
+        id: 38,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p38_img,
+        new_price: 93.0,
+        old_price: 133.0,
+      },
+      {
+        id: 39,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p39_img,
+        new_price: 87.0,
+        old_price: 127.0,
+      },
+      {
+        id: 40,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p40_img,
+        new_price: 95.0,
+        old_price: 135.0,
+      },
+      {
+        id: 41,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p41_img,
+        new_price: 88.0,
+        old_price: 128.0,
+      },
+      {
+        id: 42,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p42_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 43,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p43_img,
+        new_price: 89.0,
+        old_price: 129.0,
+      },
+      {
+        id: 44,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p44_img,
+        new_price: 96.0,
+        old_price: 136.0,
+      },
+      {
+        id: 45,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p45_img,
+        new_price: 87.0,
+        old_price: 127.0,
+      },
+      {
+        id: 46,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p46_img,
+        new_price: 90.0,
+        old_price: 130.0,
+      },
+      {
+        id: 47,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p47_img,
+        new_price: 95.0,
+        old_price: 135.0,
+      },
+      {
+        id: 48,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p48_img,
+        new_price: 88.0,
+        old_price: 128.0,
+      },
+      {
+        id: 49,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p49_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 50,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p50_img,
+        new_price: 94.0,
+        old_price: 134.0,
+      },
+      {
+        id: 51,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p51_img,
+        new_price: 93.0,
+        old_price: 133.0,
+      },
+      {
+        id: 52,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p52_img,
+        new_price: 89.0,
+        old_price: 129.0,
+      },
+      {
+        id: 53,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p53_img,
+        new_price: 91.0,
+        old_price: 131.0,
+      },
+      {
+        id: 54,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p54_img,
+        new_price: 95.0,
+        old_price: 135.0,
+      },
+      {
+        id: 55,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p55_img,
+        new_price: 87.0,
+        old_price: 127.0,
+      },
+      {
+        id: 56,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p56_img,
+        new_price: 94.0,
+        old_price: 134.0,
+      },
+      {
+        id: 57,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p57_img,
+        new_price: 93.0,
+        old_price: 133.0,
+      },
+      {
+        id: 58,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p58_img,
+        new_price: 92.0,
+        old_price: 132.0,
+      },
+      {
+        id: 59,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "dolls",
+        image: p59_img,
+        new_price: 90.0,
+        old_price: 130.0,
+      },
+      {
+        id: 60,
+        name: "Boys Orange Colourblocked Hooded Sweatshirt",
+        category: "tools",
+        image: p60_img,
+        new_price: 5.0,
+        old_price: 8.5,
+      },
+      {
+        id: 61,
+        name: "Boys Orange Colourblocked Hooded Sweatshirt",
+        category: "tools",
+        image: p61_img,
+        new_price: 6.0,
+        old_price: 9.5,
+      },
+      {
+        id: 62,
+        name: "Boys Orange Colourblocked Hooded Sweatshirt",
+        category: "tools",
+        image: p62_img,
+        new_price: 4.5,
+        old_price: 7.0,
+      },
+      {
+        id: 63,
+        name: "Boys Orange Colourblocked Hooded Sweatshirt",
+        category: "tools",
+        image: p63_img,
+        new_price: 5.5,
+        old_price: 8.0,
+      },
+      {
+        id: 64,
+        name: "Boys Orange Colourblocked Hooded Sweatshirt",
+        category: "tools",
+        image: p64_img,
+        new_price: 6.5,
+        old_price: 9.0,
+      },
+      
+      
+ 
+  
+];
+
+export default all_product;
