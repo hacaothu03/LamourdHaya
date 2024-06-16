3d91df9f1203ec0a497a6ac6d8a8d088f5aa5aa7
Ha Cao Thu <ha.caothu.03@gmail.com>
2024-06-16 16:03:04 +0700

    signup update

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -4,17 +4,50 @@ import './CSS/LoginSignup.css'
 const LoginSignup = () => {
 
     const [state,setState] = useState("Login");
+    const[formData,setFormData] = useState({
+        username:"",
+        password:"",email:""
+    })
+
+    const changeHandler = (e)=>{
+        setFormData({...formData,[e.target.name]:e.target.value})
+    }
+
+    const login = async()=>{
+        console.log("Login function executed", formData);
+    }
+
+    const signup = async()=>{
+        console.log("signup function executed", formData);
+        let responseData;
+        await fetch('http://localhost:4000/signup',{
+            method:'POST',
+            headers:{
+                Accept:'application/form-data',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(formData),
+        }).then((response)=> response.json()).then((data)=>responseData=data)
+
+        if(responseData.success){
+            localStorage.setItem('auth-token',responseData.token);
+            window.location.replace("/");
+        }
+        else{
+            alert(responseData.errors)
+        }
+    }
 
     return (
         <div className='loginsignup'>
             <div className="loginsignup-container">
                 <h1>{state}</h1>
                 <div className="loginsignup-fields">
-                    {state==="Sign Up"?<input type="text" placeholder='Your Name'/>:<></>}
-                    <input type="email" placeholder='Email Address'/>
-                    <input type="password" placeholder='Password'/>
+                    {state==="Sign Up"?<input name = "username" value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name'/>:<></>}
+                    <input name="email" value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address'/>
+                    <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password'/>
                 </div>
-                <button>Continue</button>
+                <button onClick={()=>{state==="Login"?login():signup()}} >Continue</button>
                 {state==="Sign Up"
                 ?<p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}} >Login here</span></p>
                 :<p className='loginsignup-login'>Create an account? <span onClick={()=>setState("Sign Up")} >Click here</span></p>}
